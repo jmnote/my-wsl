704e19c2a5850b1da0dd09d5f901f58732018a3e
jmnote <opcore@gmail.com>
2024-05-30 20:15:32 +0900

    docker

--- scripts/install/docker.sh
+++ scripts/install/docker.sh
@@ -4,10 +4,7 @@
 which docker && exit
 
 # install
-curl -fsSL https://get.docker.com -o get-docker.sh
-sed 's|sleep [0-9]\+|sleep 0|g'   -i get-docker.sh
-sh get-docker.sh
-rm -f   get-docker.sh
+curl -fsSL https://get.docker.com | sed 's|sleep [0-9]\+|sleep 0|g' | sh
 grep docker ~/.bashrc || (echo 'service docker start' >> ~/.bashrc)
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 
